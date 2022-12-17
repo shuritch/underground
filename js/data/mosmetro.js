3b9ae9efb575a83896dfa40f1663bd1ed9b53208
Alexander Ivanov <95919539+sashapop10@users.noreply.github.com>
2022-12-18 00:18:25 +0300

    Update mosmetro.js

--- js/data/mosmetro.js
+++ js/data/mosmetro.js
@@ -1,9 +1,4 @@
-//
-// List with coords of Moscow Metro lines and stations
-//  [[ftype, ftag, coords, label, centerof, ismap],...]
-// Three points with coords and 'Q' literal implements to bezierCurveTo (...) method.
-// egax@bk.ru, 2013-20.
-//
+
 var MLINES=[
 ['moskva_canal','mc',[
   [-120,29],
